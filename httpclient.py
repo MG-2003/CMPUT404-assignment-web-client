64a468e22fe1e9583c003e2cebe8317d3eac8bbd
MG-2003 <mgadhia@ualberta.ca>
2023-02-10 00:33:10 -0700

    assign2

--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright 2023 Mehul Gadhia, Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
-import urllib.parse
+from urllib.parse import urlparse
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -41,13 +41,14 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        return int(data.split('\r\n')[0].split(' ')[1])
 
     def get_headers(self,data):
         return None
 
     def get_body(self, data):
-        return None
+        body = data.split('\r\n\r\n')[-1]
+        return body if body else ""
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -68,13 +69,49 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        DEFAULT_PORTS = {"http": 80, "https": 443}
+        parsedUrl = urlparse(url)
+
+        scheme, host, port, path = parsedUrl.scheme, parsedUrl.hostname, parsedUrl.port, parsedUrl.path
+        path = path if path else '/'
+        port = port if port else DEFAULT_PORTS[scheme]
+        print("URL->", url, "ARGS->", args, "PORT", port)
+        print("DATA->", f"GET {path} HTTP/1.1\r\nHost: {host}\r\n\r\n")
+
+        self.connect(host, port)
+        self.sendall(f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n")
+
+        self.socket.shutdown(socket.SHUT_WR)
+        response = self.recvall(self.socket)
+        print("RESPONSE", response)
+        code = self.get_code(response)
+        body = self.get_body(response)
+
+        self.close()
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        DEFAULT_PORTS = {"http": 80, "https": 443}
+        CONTENT_TYPE = "application/x-www-form-urlencoded"
+        parsedUrl = urlparse(url)
+
+        scheme, host, port, path = parsedUrl.scheme, parsedUrl.hostname, parsedUrl.port, parsedUrl.path
+        port = port if port else DEFAULT_PORTS[scheme]
+        body = [] if args else ""
+        if args:
+            for arg in args:
+                body.append(f"{arg}={args[arg]}")
+            body = "&".join(body)
+
+        self.connect(host, port)
+        self.sendall(f"POST {path} HTTP/1.1\r\nHost: {host}\r\nContent-Type: {CONTENT_TYPE}\r\nContent-Length: {len(body)}\r\n\r\n{body}")
+
+        self.socket.shutdown(socket.SHUT_WR)
+        response = self.recvall(self.socket)
+        code = self.get_code(response)
+        body = self.get_body(response)
+
+        self.close()
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
